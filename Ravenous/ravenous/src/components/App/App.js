e88a6e26f172a8ac37aa5afc8420f6547b721b1f
lemonshushu <75408295+lemonshushu@users.noreply.github.com>
2021-02-12 17:00:21 +0900

    Ravenous - Initial Commit

---
+++ Ravenous/ravenous/src/components/App/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
+  }
+}
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [];
